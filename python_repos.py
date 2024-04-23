14375befac76f7169cb5a4c90498d24a73dc8c12
Ilya Savin <ilja.savin18@yandex.ru>
2024-04-23 14:20:56 +0300

    вывод на диграмм ссылок

--- python_repos.py
+++ python_repos.py
@@ -15,10 +15,15 @@ print('Total repositories: ', response_dict['total_count'])
 
 # Анализ информации о репозиториях
 repo_dicts = response_dict['items']
-names, stars = [], []
+names, plot_dicts = [], []
 for repo_dict in repo_dicts:
     names.append(repo_dict['name'])
-    stars.append(repo_dict['stargazers_count'])
+    plot_dict = {
+        'value': repo_dict['stargazers_count'],
+        'label': repo_dict['description'],
+        'xlink': repo_dict['html_url'],
+        }
+    plot_dicts.append(plot_dict)
 
 # Построение визуализации.
 my_style = LS('#333366', base_style=LCS)
@@ -35,5 +40,5 @@ chart = pygal.Bar(my_config, style = my_style)
 chart.title = 'Most-Starred Python Projects on GitHub'
 chart.x_labels = names
 
-chart.add('', stars)
+chart.add('', plot_dicts)
 chart.render_to_file('python_repos.svg')
